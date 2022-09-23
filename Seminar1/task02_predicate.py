c649b232722544dfbc3a2218cccff5262f5b141b
Panov Timofey <kirito.panov57@Gmail.com>
2022-09-24 00:26:25 +0300

    добавлены решения к домашнему заданию по 1 семинару по python

---
+++ Seminar1/task02_predicate.py
@@ -0,0 +1,23 @@
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def input_numbers(x):
+    xyz = ["x", "y", "z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def check_predicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] or not x[1]  or not x[2]
+    result = left == right
+    return result
+
+
+statement = input_numbers(3)
+
+if check_predicate(statement) == True:
+    print("Утверждение истинно")
+else:
+    print("Утверждение ложно")
---
+++ Seminar1/task04_coord_zone.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+def check_zone(chetv):
+    match chetv:
+        case 1 :
+            print("x > 0 and y > 0")
+        case 2:
+            print(" x < 0 and y > 0")
+        case 3:
+            print("x < 0 and y < 0")      
+        case 4:
+            print("x > 0 and y < 0")
+
+input_zone = int(input("Введите номер четверти: "))
+check_zone(input_zone)
+
+
+
+
+
