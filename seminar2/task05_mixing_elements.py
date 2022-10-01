4f3587734c79ff7bcc8c651a0cec7caff14828a3
Panov Timofey <kirito.panov57@Gmail.com>
2022-10-01 21:01:19 +0300

    ДОБАВЛЕНЫ ИЗМЕНЕНИЯ В 5 ЗАДАНИИ

--- seminar2/task05_mixing_elements.py
+++ seminar2/task05_mixing_elements.py
@@ -1,12 +1,17 @@
 # Реализуйте алгоритм перемешивания списка.
+
 import random
 random_list = random.sample(range(-10, 10), 10)
-def shuffle(random_list):
-    random_list = list(random_list)
-    random.shuffle(random_list)
+def mix_list(random_list):
+    random_list = random_list[:]
+    list_length = len(random_list)
+    for i in range(list_length):
+        index_aleatory = random.randint(0, list_length - 1)
+        temp = random_list[i]
+        random_list[i] = random_list[index_aleatory]
+        random_list[index_aleatory] = temp
     return random_list
 
-
-mixing_list = shuffle(random_list)
-print(f'Первоначальный список: {random_list}')
-print(f'Перемешанный список: {mixing_list}')
+mixed_list = mix_list(random_list)
+print(f'Изначальный список:  {random_list}')
+print(f'Перемешанный список:  {mixed_list}')
